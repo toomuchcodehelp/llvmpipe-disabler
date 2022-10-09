b2d5306c0e4b0677168fc54a70231229139d7b3e
toomuchcodehelp <111477980+toomuchcodehelp@users.noreply.github.com>
2022-10-10 09:51:59 +1000

    add root check

--- llvmpipebomb.sh
+++ llvmpipebomb.sh
@@ -1,4 +1,11 @@
-echo "killing llvmpipe"
-sudo rm /usr/share/vulkan/icd.d/lvp_icd.i686.json
-sudo rm /usr/share/vulkan/icd.d/lvp_icd.x86_64.json
-echo "Done! You might need to do a reboot..."
+if (whoami == root)
+  then
+    echo "deleting llvmpipe..."
+    sudo rm /usr/share/vulkan/icd.d/lvp_icd.i686.json
+    sudo rm /usr/share/vulkan/icd.d/lvp_icd.x86_64.json
+    echo "Done! You might need to do a reboot..."
+  exit
+  else (whoami != root)
+    then echo "you need to be root to delete these files"
+    exit
+fi
